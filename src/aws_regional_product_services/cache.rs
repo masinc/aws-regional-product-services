db2a5a3f473e6939509586725ad711d14c77f597
masinc <masinc000@gmail.com>
2023-09-16 17:05:53 +0900

    fix: race condition

--- src/aws_regional_product_services/cache.rs
+++ src/aws_regional_product_services/cache.rs
@@ -10,9 +10,10 @@ impl Cache {
     pub fn new() -> anyhow::Result<Self> {
         let p = get_data_json_path();
 
-        if p.exists() {
+        if let Ok(p) = std::fs::read_to_string(p) {
             let data = std::fs::read_to_string(p)?;
             let data = serde_json::from_str::<AwsRegionalProductServices>(&data)?;
+
             Ok(Self { data: Some(data) })
         } else {
             Ok(Self { data: None })
