475585013f2f3384eb8bd9c652b7eedea9d562be
masinc <masinc000@gmail.com>
2023-09-16 17:08:09 +0900

    --amend

--- src/aws_regional_product_services/cache.rs
+++ src/aws_regional_product_services/cache.rs
@@ -10,7 +10,7 @@ impl Cache {
     pub fn new() -> anyhow::Result<Self> {
         let p = get_data_json_path();
 
-        if let Ok(p) = std::fs::read_to_string(p) {
+        if let Ok(data) = std::fs::read_to_string(p) {
             let data = serde_json::from_str::<AwsRegionalProductServices>(&data)?;
 
             Ok(Self { data: Some(data) })
